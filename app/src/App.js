5e6d30d0ce7f611a7d7df4c815f4687a5dd6f3eb
Paul Murff <titan.murff@gmail.com>
2017-07-04 12:20:50 -0600

    done class project

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Calculator from './Calculator'
+
+class App extends Component {
+  render() {
+    return (
+      <Calculator/>
+    );
+  }
+}
+
+export default App;
+
+//  like prototype needs no function key word, destructuring - same name key prop, shortcut no need for key value pair if the same  , make sure not to wrap in curly braces when your not supposed to. 
